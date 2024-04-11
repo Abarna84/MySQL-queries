fb7d54d143df3b4a63bc2ea123fd8bbd45133b80
Abarna84 <156940515+Abarna84@users.noreply.github.com>
2024-04-11 17:29:32 -0400

    Time: 859 ms (24.24%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT 
+    id, 
+    IF(id % 2 = 1, LEAD(student, 1, student) OVER(ORDER BY id), LAG(student) OVER(ORDER BY id)) AS student
+FROM 
+    Seat;
